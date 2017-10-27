f40ca9e20a0e36c3fa0ce9d3c94f1bb94f92175f
CNine <cnine229@gmail.com>
2017-10-27 18:49:37 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+const path          = require('path')
+const logger        = require('morgan')
+const express       = require('express')
+const favicon       = require('serve-favicon')
+const bodyParser    = require('body-parser')
+const cookieParser  = require('cookie-parser')
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const session = require('./lib/session')
+const lang    = require('./lib/lang')
+const site    = require('./lib/site')
 
-var app = express();
+const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// # View engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// # Middleware setup
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(logger('dev'))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
 
-app.use('/', index);
-app.use('/users', users);
+// # Load lib
+session.use(app)
+site.use(app)
+lang.use(app)
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
-module.exports = app;
+module.exports = app
